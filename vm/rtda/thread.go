3eb9f24398e7c025d772aa5dd2b719e0cf69b56a
zexho <zouzhihao994@gmail.com>
2020-08-17 22:15:53 +0800

    [refactor] add thread.go

---
+++ vm/rtda/thread.go
@@ -0,0 +1,22 @@
+package rtda
+
+type Thread struct {
+	pc    int
+	stack *Stack
+}
+
+func NewThread() *Thread {
+	return &Thread{stack: newStack(1024)}
+}
+
+func (self *Thread) PushFrame(frame *Frame) {
+	self.stack.push(frame)
+}
+
+func (self *Thread) PopFrame(frame *Frame) {
+	return self.stack.pop()
+}
+
+func (self *Thread) CurrentFrame() *Frame {
+	return self.stack.top()
+}
