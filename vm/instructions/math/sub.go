738fe2b8de10ce0d6a4f25570b5df6b1b030ab27
zexho <zouzhihao994@gmail.com>
2020-08-23 17:49:11 +0800

    [update] add new sub instruction

---
+++ vm/instructions/math/sub.go
@@ -0,0 +1,54 @@
+package math
+
+import (
+	"../../instructions/base"
+	"../../rtda"
+)
+
+type ISUB struct {
+	base.NoOperandsInstruction
+}
+
+type LSUB struct {
+	base.NoOperandsInstruction
+}
+
+type DSUB struct {
+	base.NoOperandsInstruction
+}
+
+type FSUB struct {
+	base.NoOperandsInstruction
+}
+
+func (self ISUB) Execute(frame rtda.Frame) {
+	stack := frame.OperandStack()
+	v1 := stack.PopInt()
+	v2 := stack.PopInt()
+	subResult := v2 - v1
+	stack.PushInt(subResult)
+}
+
+func (self LSUB) Execute(frame rtda.Frame) {
+	stack := frame.OperandStack()
+	v1 := stack.PopLong()
+	v2 := stack.PopLong()
+	subResule := v2 - v1
+	stack.PushLong(subResule)
+}
+
+func (self DSUB) Execute(frame rtda.Frame) {
+	stack := frame.OperandStack()
+	v1 := stack.PopDouble()
+	v2 := stack.PopDouble()
+	subResult := v2 - v1
+	stack.PushDouble(subResult)
+}
+
+func (self FSUB) Execute(frame rtda.Frame) {
+	stack := frame.OperandStack()
+	v1 := stack.PopFloat()
+	v2 := stack.PopFloat()
+	subResult := v2 - v1
+	stack.PushFloat(subResult)
+}
