7fb19b5efc8d8a47e6004652c6be4078702691b9
zouzhihao <1035281205@qq.com>
2020-08-25 23:18:03 +0800

    [update] supplement if_icmp.go

--- vm/instructions/comparisons/if_icmp.go
+++ vm/instructions/comparisons/if_icmp.go
@@ -28,3 +28,48 @@ func (self *IF_ICMPNE) Execute(frame *rtda.Frame) {
 		base.Branch(frame, self.Offset)
 	}
 }
+
+func (self *IF_ICMPEQ) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	val2 := stack.PopInt()
+	val1 := stack.PopInt()
+	if val1 == val2 {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+func (self *IF_ICMPGE) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	val2 := stack.PopInt()
+	val1 := stack.PopInt()
+	if val1 >= val2 {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+func (self *IF_ICMPGT) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	val2 := stack.PopInt()
+	val1 := stack.PopInt()
+	if val1 > val2 {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+func (self *IF_ICMPLT) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	val2 := stack.PopInt()
+	val1 := stack.PopInt()
+	if val1 < val2 {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+func (self *IF_ICMPLE) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	val2 := stack.PopInt()
+	val1 := stack.PopInt()
+	if val1 <= val2 {
+		base.Branch(frame, self.Offset)
+	}
+}
