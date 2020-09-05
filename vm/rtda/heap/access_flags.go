8c29174c05cdb620884047bb6ebc110b57bde7e9
zexho <zouzhihao994@gmail.com>
2020-09-05 14:26:43 +0800

    add access_flags const

---
+++ vm/rtda/heap/access_flags.go
@@ -0,0 +1,40 @@
+package heap
+
+const (
+	// public
+	ACC_PUBLIC = 0x0001
+	// private
+	ACC_PRIVATE = 0x0002
+	// protected
+	ACC_PROTECTED = 0x0004
+	// static
+	ACC_STATIC = 0x0008
+	// final
+	ACC_FINAL = 0x0010
+	// synchronized
+	ACC_SYNCHRONIZED = 0x0020
+	// volatile
+	ACC_VOLATILE = 0x0020
+	// super
+	ACC_SUPER = 0x0020
+	// bridge
+	ACC_BRIDGE = 0x0040
+	// transient
+	ACC_TRANSIENT = 0x0040
+	// varargs
+	ACC_VARARGS = 0x0080
+	// native
+	ACC_NATIVE = 0x0100
+	// interface
+	ACC_INTERFACE = 0x0200
+	// abstract
+	ACC_ABSTRACT = 0x0400
+	// strict
+	ACC_STRICT = 0x0800
+	// synthetic
+	ACC_SYNTHETIC = 0x1000
+	// annotation
+	ACC_ANNOTATION = 0x2000
+	// enum
+	ACC_ENUM = 0x4000
+)
