ccdb3354fa9fb95feab5c0d4befc02910e5f1f76
zexho <zouzhihao994@gmail.com>
2020-09-18 20:06:09 +0800

    添加ifcond指令

--- vm/instructions/comparisons/ifcond.go
+++ vm/instructions/comparisons/ifcond.go
@@ -10,35 +10,70 @@ if equals
 */
 type IFEQ struct{ base.BranchInstruction }
 
+func (self *IFEQ) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	// if val equals zero than jump
+	if val == 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
+
 /*
 if not equals
 */
 type IFNE struct{ base.BranchInstruction }
+
+func (self *IFNE) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val != 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
 
 /*
 if （left bigger than right）
 */
 type IFLT struct{ base.BranchInstruction }
 
+func (self *IFLT) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val < 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
+
 /*
 if(left bigger than right or equals)
 */
 type IFLE struct{ base.BranchInstruction }
+
+func (self *IFLE) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val <= 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
 
 /*
 if(left smaller than right)
 */
 type IFGT struct{ base.BranchInstruction }
 
+func (self *IFGT) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val > 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
+
 /*
 if(left smaller than right or equals)
 */
 type IFGE struct{ base.BranchInstruction }
 
-func (self *IFEQ) Execute(frame *rtda.Frame) {
+func (self *IFGE) Execute(frame *rtda.Frame) {
 	val := frame.OperandStack().PopInt()
-	// if val equals zero than jump
-	if val == 0 {
+	if val >= 0 {
 		base.Branch(frame, self.Offset)
 	}
 }
