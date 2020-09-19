7637adb9cf789c251cecad83ce43569e51c11702
zouzhihao-994 <zouzhihao994@gmail.com>
2020-09-19 16:23:10 +0800

    添加if_acmpne指令

--- vm/instructions/comparisons/if_acmp.go
+++ vm/instructions/comparisons/if_acmp.go
@@ -13,10 +13,19 @@ type IF_ACMPNE struct{ base.BranchInstruction }
 弹出两个引用对象，如果相同就跳转
 */
 func (self *IF_ACMPEQ) Execute(frame *rtda.Frame) {
+	if _acmp(frame) {
+		base.Branch(frame, self.Offset)
+	}
+}
+
+func (self *IF_ACMPNE) Execute(frame *rtda.Frame) {
+	if !_acmp(frame) {
+		base.Branch(frame, self.Offset)
+	}
+}
+func _acmp(frame *rtda.Frame) bool {
 	stack := frame.OperandStack()
 	ref2 := stack.PopRef()
 	ref1 := stack.PopRef()
-	if ref1 == ref2 {
-		base.Branch(frame, self.Offset)
-	}
+	return ref1 == ref2 // todo
 }
