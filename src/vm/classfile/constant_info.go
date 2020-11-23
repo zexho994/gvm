b5165189f02cb56cbb5b43d9f4b5659656edff93
zexho <zouzhihao994@gmail.com>
2020-11-23 22:14:10 +0800

    rename for constant_info

--- src/vm/classfile/constant_info.go
+++ src/vm/classfile/constant_info.go
@@ -6,16 +6,8 @@ tag
 tag用来区分常量类型
 */
 const (
-	// 类或者接口的符号引用
-	ConstantClass = 7
-	// 字段的符号引用
-	ConstantFieldRef = 9
-	// 类中方法的符号引用
-	ConstantMethodRef = 10
-	// 接口中方法的符号引用
-	ConstantInterfaceMethodRef = 11
-	// 字符串类型字面量
-	ConstantString = 8
+	// UTF-8编码的字符串
+	ConstantUtf8 = 1
 	// 整形字面量
 	ConstantInteger = 3
 	// 浮点数字面量
@@ -24,10 +16,18 @@ const (
 	ConstantLong = 5
 	// 双精度浮点型字面量
 	ConstantDouble = 6
+	// 类或者接口的符号引用
+	ConstantClass = 7
+	// 字符串类型字面量
+	ConstantString = 8
+	// 字段的符号引用
+	ConstantFieldRef = 9
+	// 类中方法的符号引用
+	ConstantMethodRef = 10
+	// 接口中方法的符号引用
+	ConstantInterfaceMethodRef = 11
 	// 字段或者方法的部分符号引用
 	ConstantNameAndType = 12
-	// UTF-8编码的字符串
-	ConstantUtf8 = 1
 	// 方法句柄
 	ConstantMethodHandle = 15
 	// 方法类型
