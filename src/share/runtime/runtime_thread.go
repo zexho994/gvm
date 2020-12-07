03bf1b7cc5665cd0a06eb8525be80514740f1dab
zexho <zouzhihao994@gmail.com>
2020-12-07 20:17:12 +0800

    add runtime_thread.go

---
+++ src/share/runtime/runtime_thread.go
@@ -0,0 +1,33 @@
+package runtime
+
+// 映射到java中的一个thread todo
+type Thread struct {
+	pc    int
+	stack *Stack
+}
+
+func (t *Thread) PC() int {
+	return t.pc
+}
+
+func (t *Thread) SetPC(newPc int) {
+	t.pc = newPc
+}
+
+func (t *Thread) PushFrame(newFrame *Frame) {
+	t.stack.Push(newFrame)
+}
+
+func (t *Thread) PopFrame() *Frame {
+	return t.stack.Pop()
+}
+
+func (t *Thread) Frame() *Frame {
+	return t.stack.Peek()
+}
+
+func newThread() *Thread {
+	return &Thread{
+		stack: NewStack(1024),
+	}
+}
