030ce306bf0f5b00a75722bc6b0b33efb31c82aa
zouzhihao-994 <zouzhihao994@gmail.com>
2020-12-19 21:52:09 +0800

    add dup instructions

--- src/share/instructions/instrctions_factory.go
+++ src/share/instructions/instrctions_factory.go
@@ -5,9 +5,16 @@ import (
 	"github.com/zouzhihao-994/gvm/src/share/instructions/base"
 	"github.com/zouzhihao-994/gvm/src/share/instructions/control"
 	"github.com/zouzhihao-994/gvm/src/share/instructions/references"
+	"github.com/zouzhihao-994/gvm/src/share/instructions/stack"
 )
 
 var (
+	dup          = &stack.Dup{}
+	dup_x1       = &stack.Dup_X1{}
+	dup_x2       = &stack.Dup_X2{}
+	dup2         = &stack.Dup2{}
+	dup2_x1      = &stack.Dup2_X1{}
+	dup2_x2      = &stack.Dup2_X2{}
 	voidReturn   = &control.RETURN{}           //177
 	getStatic    = &references.GET_STATIC{}    // 178
 	invokeStatic = &references.INVOKE_STATIC{} // 184
@@ -16,6 +23,18 @@ var (
 
 func NewInstruction(opcode byte) base.Base_Instruction {
 	switch opcode {
+	case 0x59:
+		return dup
+	case 0x5a:
+		return dup_x1
+	case 0x5b:
+		return dup_x2
+	case 0x5c:
+		return dup2
+	case 0x5d:
+		return dup2_x1
+	case 0x5e:
+		return dup2_x2
 	case 0xb1:
 		return voidReturn
 	case 0xb2:
