c8b12321463903f0b507393935b668664c623a75
zexho <zouzhihao994@gmail.com>
2020-12-19 09:51:18 +0800

    new class file

---
+++ java/src/algorithm/ALU.java
@@ -0,0 +1,6 @@
+public class ALU {
+    public static void main(String[] args) {
+        Add a = new Add();
+        a.add(11,39);
+    }
+}
---
+++ java/src/algorithm/Add.java
@@ -0,0 +1,6 @@
+public class Add {
+    public int add(int n1, int n2) {
+        int n3 = n1 + n2;
+        return n3;
+    }
+}
