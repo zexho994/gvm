d869e3f19ddc405349de75719d4f8bea0aa42420
zexho <zouzhihao994@gmail.com>
2020-12-21 20:05:41 +0800

    add AssertFalse

--- src/share/exception/assert.go
+++ src/share/exception/assert.go
@@ -1,7 +1,15 @@
 package exception
 
+// the flag must be true
 func AssertTrue(flag bool, msg string) {
 	if !flag {
+		panic(GvmError{Msg: msg})
+	}
+}
+
+// the flag must be false
+func AssertFalse(flag bool, msg string) {
+	if flag {
 		panic(GvmError{Msg: msg})
 	}
 }
