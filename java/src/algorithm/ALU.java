765d371bb3bfce131a87c714538c44233297a55b
zexho <zouzhihao994@gmail.com>
2020-12-24 08:31:17 +0800

    add Substraction.java

--- java/src/algorithm/ALU.java
+++ java/src/algorithm/ALU.java
@@ -2,5 +2,7 @@ public class ALU {
     public static void main(String[] args) {
         Add a = new Add();
         a.add(11,39);
+        Substraction s = new Substraction();
+        s.sub(100,50);
     }
 }
---
+++ java/src/algorithm/Substraction.java
@@ -0,0 +1,5 @@
+public class Substraction {
+    public void sub(int n1, int n2) {
+        int n3 = n1 - n2;
+    }
+}
