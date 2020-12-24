0983aa89dd882849808c1e6f41cb367a7330afaa
zexho <zouzhihao994@gmail.com>
2020-12-24 08:37:50 +0800

    add sub.go

---
+++ src/share/instructions/math/sub.go
@@ -0,0 +1,54 @@
+package math
+
+import (
+	"github.com/zouzhihao-994/gvm/src/share/instructions/base"
+	"github.com/zouzhihao-994/gvm/src/share/runtime"
+)
+
+type ISUB struct {
+	base.InstructionIndex0
+}
+
+type LSUB struct {
+	base.InstructionIndex0
+}
+
+type DSUB struct {
+	base.InstructionIndex0
+}
+
+type FSUB struct {
+	base.InstructionIndex0
+}
+
+func (sub *ISUB) Execute(frame *runtime.Frame) {
+	stack := frame.OperandStack()
+	v1 := stack.PopInt()
+	v2 := stack.PopInt()
+	subResult := v2 - v1
+	stack.PushInt(subResult)
+}
+
+func (sub *LSUB) Execute(frame *runtime.Frame) {
+	stack := frame.OperandStack()
+	v1 := stack.PopLong()
+	v2 := stack.PopLong()
+	subResule := v2 - v1
+	stack.PushLong(subResule)
+}
+
+func (sub *DSUB) Execute(frame *runtime.Frame) {
+	stack := frame.OperandStack()
+	v1 := stack.PopDouble()
+	v2 := stack.PopDouble()
+	subResult := v2 - v1
+	stack.PushDouble(subResult)
+}
+
+func (sub *FSUB) Execute(frame *runtime.Frame) {
+	stack := frame.OperandStack()
+	v1 := stack.PopFloat()
+	v2 := stack.PopFloat()
+	subResult := v2 - v1
+	stack.PushFloat(subResult)
+}
