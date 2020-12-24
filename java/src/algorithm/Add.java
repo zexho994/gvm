feb8abb306119271525681b8868cd4782da5b085
zexho <zouzhihao994@gmail.com>
2020-12-24 08:52:52 +0800

    new test case

--- java/src/algorithm/Add.java
+++ java/src/algorithm/Add.java
@@ -2,4 +2,14 @@ public class Add {
     public void add(int n1, int n2) {
         int n3 = n1 + n2;
     }
+
+    public void add1(){
+        int i = 0;
+        int j = i++;
+    }
+
+    public void add2(){
+        int i = 0;
+        int k = ++i;
+    }
 }
