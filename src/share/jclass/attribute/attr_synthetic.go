60cf0c4bd3e99c7af727d52f42b3b3ed439c4851
zouzhihao-994 <zouzhihao994@gmail.com>
2020-12-27 10:18:33 +0800

    support synthetic attribute

---
+++ src/share/jclass/attribute/attr_synthetic.go
@@ -0,0 +1,17 @@
+package attribute
+
+import "github.com/zouzhihao-994/gvm/src/share/classfile"
+
+type Attr_Synthetic struct {
+	nameIdx uint16
+	name    string
+	length  uint32
+}
+
+func (attr Attr_Synthetic) parse(reader *classfile.ClassReader) {
+	// nothing
+}
+
+func (attr Attr_Synthetic) Name() string {
+	return ""
+}
