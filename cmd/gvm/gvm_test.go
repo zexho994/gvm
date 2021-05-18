8eab76049c1048e3a971e703e52c67377ba7614a
zexho <zouzhihao994@gmail.com>
2021-05-18 14:09:49 +0800

    add test case for cmd/gvm

---
+++ cmd/gvm/gvm_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/zouzhihao-994/gvm/congifuration"
+	"testing"
+)
+
+func Test_startJVM(t *testing.T) {
+	type args struct {
+		className     string
+		jrePath       string
+		userClassPath string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{name: "test1", args: args{className: "AlgorithmTest", jrePath: congifuration.JrePath, userClassPath: congifuration.UserClassPath}},
+		{name: "test2", args: args{className: "LogicTest", jrePath: congifuration.JrePath, userClassPath: congifuration.UserClassPath}},
+		{name: "test3", args: args{className: "PrintFieldsTest", jrePath: congifuration.JrePath, userClassPath: congifuration.UserClassPath}},
+		{name: "test4", args: args{className: "PrintStaticTest", jrePath: congifuration.JrePath, userClassPath: congifuration.UserClassPath}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+		})
+	}
+}
