f925995309dd24a5662974b1e4051872f8d39f5c
zexho <zouzhihao994@gmail.com>
2021-08-24 20:06:46 +0800

    update: add inst 0xc0 checkcase

--- java/PrintOut.java
+++ java/PrintOut.java
@@ -5,7 +5,9 @@
 public class PrintOut {
 
     public static void main(String[] args) {
-        System.out.println("11_22_33");
+        int x = 1;
+        int y = 2;
+        int c = x + y;
     }
 
 }
