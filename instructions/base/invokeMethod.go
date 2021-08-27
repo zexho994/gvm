ed3809b9b113b5a91394129654e77bf0e9b8872f
zexho <zouzhihao994@gmail.com>
2021-08-27 09:44:09 +0800

    refactor: InvokeMethod add setArguments()

--- instructions/base/invokeMethod.go
+++ instructions/base/invokeMethod.go
@@ -10,8 +10,8 @@ import (
 // InvokeMethod 执行方法调用
 // 对于静态方法，方法参数就是声明的几个参数
 // 对于实例方法，参数要加上编译器添加的this
-// 对于本地方法，
-// 对于接口方法，
+// 对于本地方法
+// 对于接口方法
 func InvokeMethod(frame *runtime.Frame, method *klass.MethodKlass, isStatic bool) {
 	if method == nil {
 		return
@@ -21,11 +21,7 @@ func InvokeMethod(frame *runtime.Frame, method *klass.MethodKlass, isStatic bool
 	attrCode, _ := method.AttrCode()
 	newFrame := runtime.NewFrame(attrCode.MaxLocals, attrCode.MaxStack, method, invokerThread)
 
-	if isStatic {
-		setStaticArguments(frame, newFrame, method)
-	} else {
-		setVirtualArguments(frame, newFrame, method)
-	}
+	setArguments(frame, newFrame, method, isStatic)
 
 	if config.LogInvoke {
 		fmt.Printf("---- %s invoke method ->  %s.%s%s ---- \n", frame.ThisClass, method.ThisClass, method.MethodName(), method.MethodDescriptor())
@@ -34,6 +30,16 @@ func InvokeMethod(frame *runtime.Frame, method *klass.MethodKlass, isStatic bool
 	invokerThread.PushFrame(newFrame)
 }
 
+// 设置参数
+func setArguments(frame *runtime.Frame, newFrame *runtime.Frame, method *klass.MethodKlass, isStatic bool) {
+	if isStatic {
+		setStaticArguments(frame, newFrame, method)
+	} else {
+		setVirtualArguments(frame, newFrame, method)
+	}
+}
+
+// 设置虚方法的参数
 func setVirtualArguments(frame *runtime.Frame, newFrame *runtime.Frame, method *klass.MethodKlass) {
 	argSlotCount := int(method.ArgSlotCount())
 	n := argSlotCount
@@ -54,6 +60,7 @@ func setVirtualArguments(frame *runtime.Frame, newFrame *runtime.Frame, method *
 	}
 }
 
+// 设置静态方法的参数
 func setStaticArguments(frame *runtime.Frame, newFrame *runtime.Frame, method *klass.MethodKlass) {
 	argSlotCount := int(method.ArgSlotCount())
 	n := argSlotCount - 1
